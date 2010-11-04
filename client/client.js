4966e6d311553b45d2c86eee9f877ff608c201d0
Mike Zupan <mzupan@theopenskyproject.com>
2010-11-04 12:52:31 -0400

    added serverhost for the index file and also cleaned up spacing issue

--- client/client.js
+++ client/client.js
@@ -1,18 +1,16 @@
 $(document).ready(function () {
-    socket = new io.Socket('192.168.100.35', {port: 3656});
+	socket = new io.Socket(serverhost, {port: 3656});
 	socket.connect();
 	socket.on('message', function(content) {
 		if (content != "") {
 			try {
-                urls = $.parseJSON(content);
-                $('tbody#urllist').html("");
-                for (k in urls) {
-                    $('tbody#urllist').append("<tr><td>"+ urls[k]['count'] +"</td><td>"+ urls[k]['url'] +"</td></tr>");
-                }
-        	}
-	        catch(err) {
-	
-	        }
-        }
+				urls = $.parseJSON(content);
+				$('tbody#urllist').html("");
+				for (k in urls) {
+					$('tbody#urllist').append("<tr><td>"+ urls[k]['count'] +"</td><td>"+ urls[k]['url'] +"</td></tr>");
+				}
+			}
+			catch(err) {}
+		}
 	});
 });
